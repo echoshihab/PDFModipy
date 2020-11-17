172198c0a1c0ea0ba735af555477b28bef92e41d
shihab <echoshihab@gmail.com>
2020-11-16 21:50:47 -0500

    add helper function to display file path

--- UI.py
+++ UI.py
@@ -7,14 +7,14 @@ class UI:
     def __init__(self):
         self.root = Tk()
         self.root.title = ("PDFModipy")
-        self.root.geometry = ("1024X800")
+        self.root.geometry = ("200x150")
         self.select_btn = Button(self.root, text="Select File")
         self.start_page = Entry(self.root, width=10)
+        self.start_page_label = Label(self.root, text="Start Page")
+        self.end_page_label = Label(self.root, text="End Page")
+        self.file_path_label = Label(self.root)
         self.end_page = Entry(self.root, width=10)
-        self.split_btn = Button(self.root, text="Split",
-                                )
+
+        self.split_btn = Button(self.root, text="Split")
         self.merge_btn = Button(self.root, text="Merge")
         self.encrypt_btn = Button(self.root, text="Encrypt")
-
-
-# command=lambda: PDfWorker(path).split_pdf(3, 6)
--- pdfModipy.py
+++ pdfModipy.py
@@ -12,14 +12,31 @@ class PDFModipy:
 
     def display_UI(self):
         """Displays the UI"""
-        self.UI.select_btn.pack()
-        self.UI.split_btn.pack()
+        self.UI.select_btn.grid(row=1, column=0)
+
+        self.UI.start_page_label.grid(row=2)
+        self.UI.start_page.grid(row=2, column=1)
+        self.UI.end_page_label.grid(row=3)
+        self.UI.end_page.grid(row=3, column=1)
+        self.UI.split_btn.grid(row=4, column=0)
+
+        # if(len(self.pdf_worker.file_path) > 0):
+        #     print("firing")
+        #     self.UI.file_path_label.configure(text=self.pdf_worker.file_path)
+        #     self.UI.file_path_label.grid(row=0)
 
     def attach_logic(self):
         """attaches commands to buttons on UI"""
-        self.UI.select_btn.configure(command=self.pdf_worker.select_pdf)
+        self.UI.select_btn.configure(
+            command=self._show_file_path)
         self.UI.split_btn.configure(
             command=lambda: self.pdf_worker.split_pdf(3, 6))
+
+    def _show_file_path(self):
+        self.pdf_worker.select_pdf()
+        self.UI.file_path_label.configure(
+            text=self.pdf_worker.file_path)
+        self.UI.file_path_label.grid(row=0)
 
     def run_program(self):
         """runs the main program"""
